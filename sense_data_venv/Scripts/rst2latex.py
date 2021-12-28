cb854e910fdb2795fc61e6d5fc6b014015d88f53
Inna Kedege <vibhav.inna.kedege@sogeti.com>
2021-12-28 16:02:01 +0100

    module1 v1

---
+++ sense_data_venv/Scripts/rst2latex.py
@@ -0,0 +1,26 @@
+#!C:\Users\vinnaked\Desktop\Software_Skills\projects\sensor_data_decoding\python-decoding-sensor-data\sense_data_venv\Scripts\python.exe
+
+# $Id: rst2latex.py 5905 2009-04-16 12:04:49Z milde $
+# Author: David Goodger <goodger@python.org>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing LaTeX.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline
+
+description = ('Generates LaTeX documents from standalone reStructuredText '
+               'sources. '
+               'Reads from <source> (default is stdin) and writes to '
+               '<destination> (default is stdout).  See '
+               '<http://docutils.sourceforge.net/docs/user/latex.html> for '
+               'the full reference.')
+
+publish_cmdline(writer_name='latex', description=description)
